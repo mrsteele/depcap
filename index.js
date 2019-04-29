a394d9ca6718e05ade01c7022e4f144d34fe1f85
mrsteele <matt@omnionline.us>
2019-04-29 10:06:57 -0400

    chore: testing something

--- index.js
+++ index.js
@@ -1,26 +1,8 @@
 #!/usr/bin/env node
-const fs = require('fs')
-const path = require('path')
-
-console.log('NPM_CONFIG_DEPCAP', process.env.NPM_CONFIG_DEPCAP)
-
-const findAppDir = async (dir = __dirname) => {
-  if (fs.existsSync(path.resolve(dir, 'node_modules'))) {
-    return dir
-  }
-
-  const newDir = path.resolve(dir, '..')
-  if (newDir === dir) {
-    throw new Error('App root directory is not found. Are you running this as a dependency?')
-  }
-
-  return findAppDir(newDir)
-}
-
 const run = async () => {
-  const appDir = await findAppDir()
-  const pkg =
-  console.log(appDir)
+  console.log('name', process.env.npm_package_name)
+  console.log('version', process.env.npm_package_version)
+  console.log('dependencies', process.env.npm_package_dependencies)
 }
 
 run()
