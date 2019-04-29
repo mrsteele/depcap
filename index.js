37553c28b7562e110c7fdd2a9f285bed12d2ec69
mrsteele <matt@omnionline.us>
2019-04-29 16:06:21 -0400

    fix: trying another thing

--- index.js
+++ index.js
@@ -3,6 +3,21 @@ const run = async () => {
   console.log('name', process.env.npm_package_name)
   console.log('version', process.env.npm_package_version)
   console.log('dependencies', process.env.npm_package_dependencies)
+
+  const {
+    npm_package_dependencies,
+    npm_config_depcap
+  } = process.env
+
+  console.log('Testing dependency size with "depcap"...')
+  if (npm_package_dependencies) {
+    const limit = parseInt(npm_config_depcap) || 10
+    const size = Object.keys(npm_package_dependencies)
+    if (size > limit) {
+      throw new Error(`You are using "${size}" dependencies, which excedes the limit of "${limit}"`)
+    }
+  }
+
 }
 
 run()
