526d72c52e1fe3fcf550e6674ee73f7f504ea599
mrsteele <matt@omnionline.us>
2019-04-30 08:51:51 -0400

    feat: finalizing

--- index.js
+++ index.js
@@ -1,23 +1,51 @@
 #!/usr/bin/env node
-const run = async () => {
-  console.log('name', process.env.npm_package_name)
-  console.log('version', process.env.npm_package_version)
-  console.log('dependencies', process.env.npm_package_dependencies)
+const path = require('path')
 
-  const {
-    npm_package_dependencies,
-    npm_config_depcap
-  } = process.env
+const defaultLimit = 10
+const defaultLimits = {
+  dependencies: defaultLimit,
+  devDependencies: defaultLimit,
+  optionalDependencies: defaultLimit
+}
+const depArr = [
+  'dependencies',
+  'devDependencies',
+  'optionalDependencies'
+]
 
-  console.log('Testing dependency size with "depcap"...')
-  if (npm_package_dependencies) {
-    const limit = parseInt(npm_config_depcap) || 10
-    const size = Object.keys(npm_package_dependencies)
-    if (size > limit) {
-      throw new Error(`You are using "${size}" dependencies, which excedes the limit of "${limit}"`)
+const parseConfig = (config) => {
+  if (Number.isInteger(config)) {
+    return {
+      dependencies: config,
+      devDependencies: config,
+      optionalDependencies: config
+    }
+  } else if (!Array.isArray(config) && typeof config === 'object') {
+    return {
+      ...defaultLimits,
+      ...config
     }
   }
 
+  return defaultLimits
+}
+
+const run = async () => {
+  console.log('Testing dependency size with "depcap"...')
+
+  const pkg = require(path.resolve(process.env.INIT_CWD, 'package.json'))
+  const limits = parseConfig(pkg.depcap)
+  depArr.forEach(dep => {
+    if (pkg[dep]) {
+      const length = Object.keys(pkg[dep]).length
+      const limit = limits[dep]
+      if (length > limit) {
+        throw new Error(`You are using "${length}" ${dep}, which excedes the limit of "${limit}"`)
+      }
+    }
+  })
+
+  console.log('PASSED')
 }
 
 run()
